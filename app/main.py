1a77743749512f8fb0155abc92472052c929306b
John Glenn <jo.devera.up@gmail.com>
2025-12-30 14:07:43 +0800

    Update main.py

--- app/main.py
+++ app/main.py
@@ -20,8 +20,9 @@ app = FastAPI(title="F.L.A.M.E.S Backend")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "https://your-frontend.vercel.app",
-        "http://localhost:3000",
+        "https://flames-testing.vercel.app/",
+        "https://flames-backend-hbu0.onrender.com",
+        "http://localhost",
         "http://127.0.0.1:5500"
     ],
     allow_credentials=True,
